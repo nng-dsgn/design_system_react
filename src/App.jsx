dc0b8a294a6d490b28299c5fcc7f777400c42d5b
Núria Navarro Góngora <nng.dsgn@gmail.com>
2025-01-25 13:01:06 +0100

    feat(subtitle): add Subtitle component with size variations and prop validation
    - Implemented Subtitle component with default size ('lg') and customizable size options ('xs', 'md', 'lg').
    - Defined prop types for `children` (required) and `size` (optional with default value).
    - Added CSS classes for size variations using `--cb-text-subtitle-xs`, `--cb-text-subtitle-md`, and `--cb-text-subtitle-lg`.
    - Included validation for `size` prop using PropTypes.
    - Provided default styling in `style.css` for size-based classes.
    - Fixed warning for missing `size` prop by setting default value in the component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import "./ui-kit/atoms/icon/font-icons.css";
 import "./App.css";
+import "./ui-kit/atoms/icon/font-icons.css";
+import Subtitle from "./ui-kit/atoms/text/subtitle";
+
 
 
 // Componente Header
 function App() {
   return (
-    <header>
+    <>
       <h1>Design System React</h1>
-      <p>¡Bienvenido al Design System React!</p>
+      <Subtitle>¡Bienvenido al Design System React!</Subtitle>
       <i className="cb-icons-checkmark-circle"></i>
-
-    </header>
+      <Subtitle size="xs">Subtítulo pequeño</Subtitle>
+      <Subtitle size="md">Subtítulo mediano</Subtitle>
+      <Subtitle size="lg">Subtítulo grande</Subtitle>
+    </>
   );
 }
 
